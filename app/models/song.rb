316151254411a2ba7cfe89f2910455cb2e7c2438
Jed Peek <jedpeek@yahoo.com>
2018-08-20 23:23:22 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,5 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def artist_name
-    Artist.find_or_create_by(name: params[:artist_name])
-  end
 
-  def genre_name
-    Genre.find_or_create_by(params[:genre_name])
-  end
 end
