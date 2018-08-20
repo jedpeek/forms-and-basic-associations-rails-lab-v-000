57e0c36828530893ba7de672e9b27c1f171400d2
Jed Peek <jedpeek@yahoo.com>
2018-08-20 23:26:50 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
-    Category.find_or_create_by(name: params[:song][:artist_name])
+    Artist.find_or_create_by(name: params[:song][:artist_name])
     Artist.find_or_create_by(name: name)
   end
 
