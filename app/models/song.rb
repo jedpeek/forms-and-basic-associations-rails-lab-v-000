3bf24713373fa9d8cf19a4ffa35c46e769a6c896
Jed Peek <jedpeek@yahoo.com>
2018-08-21 00:10:09 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -25,7 +25,7 @@ class Song < ActiveRecord::Base
     def note_contents=(notes)
     notes.each do |content|
       if content.strip != ''
-        note = self.notes.create(content: content)
+        note = self.notes.build(content: content)
         self.notes << note
       end
     end
