6ff1631e3a17dff8b0ef60bdc04a91969afd02f0
Jed Peek <jedpeek@yahoo.com>
2018-08-21 00:15:42 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -25,7 +25,7 @@ class Song < ActiveRecord::Base
   def note_contents=(notes)
     notes.each do |content|
       if content != ''
-        self.notes.create(content: content)
+        self.notes.build(content: content)
       end
     end
   end
